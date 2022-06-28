040d079e84e86d1800d1b3b54f5f2c051b225573
Luis Jimenez <you@example.com>
2022-06-28 15:31:25 -0500

    Fixed problem with Uppercase and Lowercase

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passwordText = document.getElementById('password');
+var passwordText = document.querySelector('#password');
   
         /* Function to generate combination of password */
-function generateP(sign, choiceCh) {
+function generateP(sign) {
   var pass = '';
       var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 
-      'abcdefghijklmnopqrstuvwxyz0123456789@#$';
+      'abcdefghijklmnopqrstuvwxyz0123456789' + "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~" ;
 
-    for (i = 1; i <= sign; i++) {
-      var char = Math.floor(Math.random()
-       * str.length + 1);
-      pass += str.charAt(char);
+  for (i = 1; i <= sign; i++) {
+    var char = Math.floor(Math.random()
+     * str.length + 1);
+    pass += str.charAt(char);
 
-switch(choiceCh.toLowerCase()) {
-  case 'uppercase':
-    return pass.toUpperCase();
-    break;
-  case 'lowercase':
-    return pass.toLowerCase();
-    break;
-  default:
-    return pass;
-}
+    /* switch(choiceCh.toLowerCase()) {
+      case 'uppercase':
+        return pass.toUpperCase();
+      break;
+      case 'lowercase':
+        return pass.toLowerCase();
+      break;
+      default:
+      return pass;
+      }*/
 
     }
-
-        }
+    return pass;
+}
 
 
         function gfg_Run() {
@@ -38,26 +38,13 @@ switch(choiceCh.toLowerCase()) {
             passwordText.value = 'Incorrect password';
             return;
           }
-          var choiceCh = prompt('Do you want your password to have Uppercase and/or Lowercase ?');
+          //var choiceCh = prompt('Do you want your password to have Uppercase and/or Lowercase ?');
 
 
 
 
-          passwordText.value = generateP(sign, choiceCh);
+          passwordText.value = generateP(sign);
 
       }
 
       generateBtn.addEventListener("click", gfg_Run);
-/*
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-
-*/
